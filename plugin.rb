02951187d012d227c351656475f38c55d0ebc694
doserved <djoshop@hotmail.com>
2016-05-06 09:54:51 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,4 @@
 # version: 0.1
 # authors: Robin Ward
 
-register_custom_html(extraNavItem: "<li> class= "ember-view" title= "ideas with the most votes" <a href='/c/ideas/l/posted?order=votes'>Most Votes</a></li>")
+register_custom_html(extraNavItem: "<li><a href='/c/ideas/l/posted?order=votes'>Most Votes</a></li>")

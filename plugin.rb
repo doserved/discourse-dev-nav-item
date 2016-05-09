17ad14225760fea407003d1437f036ca7d572316
doserved <djoshop@hotmail.com>
2016-05-09 14:13:50 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,4 @@
 # version: 0.1
 # authors: Robin Ward
 
-register_custom_html(extraNavItem: "<li><a href='/c/ideas/l/posted?order=votes'>Most Votes</a></li>")
+register_custom_html(extraNavItem: "<li><a href='/c/ideas/l/top/yearly?order=votes'>Most Votes</a></li>")

ca12d1e2f0aab3a74d809972d72f9c4f2d773eb3
doserved <djoshop@hotmail.com>
2016-05-23 10:10:42 -0400

    Update plugin.rb

--- plugin.rb
+++ plugin.rb
@@ -3,4 +3,4 @@
 # version: 0.1
 # authors: Robin Ward
 
-register_custom_html(extraNavItem: "<li class="most-votes" title="the ideas with the most votes in the last year"><a href='/c/ideas/l/top/yearly?order=votes'>Most Votes</a></li>")
+register_custom_html(extraNavItem: "<li><a href='/c/ideas/l/top/yearly?order=votes'>Most Votes</a></li>")
